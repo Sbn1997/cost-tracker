19e33d129ef999ba89df2bf42c5e272d8a892aba
yarin1890 <104309089+yarin1890@users.noreply.github.com>
2024-05-02 19:42:51 +0300

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
+/* Yarin Ben Moshe 314939885
+Amit Rahamim 318816535
+Shahar Ben Naim 208628453 */
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
